08b44691db61dfa04d0f2e8991c02528202e63b9
ilyavk <korytov@tpm38.ru>
2025-03-29 22:04:09 +0800

    api

---
+++ 15-api-1/example/api.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type API struct {
+	router *mux.Router
+}
+
+func (api *API) Endpoints() {
+	api.router.Use(logMiddleware)
+	api.router.HandleFunc("/api/v1/books", api.books).Methods(http.MethodGet, http.MethodOptions)
+	api.router.HandleFunc("/api/v1/books", api.newBook).Methods(http.MethodPost, http.MethodGet)
+}
+
+func (api *API) books(w http.ResponseWriter, r *http.Request) {
+	err := json.NewEncoder(w).Encode(books)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (api *API) newBook(w http.ResponseWriter, r *http.Request) {
+	var b book
+	err := json.NewDecoder(r.Body).Decode(&b)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	books = append(books, b)
+}
